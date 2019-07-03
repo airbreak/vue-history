af1db7962d8f48a30239ab50d7b803771e78e959
airbreak <jiangjianming_m@163.com>
2019-07-03 17:12:50 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import List from '@/components/List'
+import Detail from '@/components/detail'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: '/history/',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: List
+    },
+    {
+      path: '/detail/:id',
+      name: 'detail',
+      component: Detail
+    }
+  ]
+})
